efb41a098ce4daa79aec16e77f1eee30828815ac
JoeSvr95 <joesaverio@hotmail.com>
2017-08-21 23:26:39 -0500

    Pago con tarjeta de crédito
    Ya funciona esa opción

--- src/controllers/cliente/TarjetaCreditoController.java
+++ src/controllers/cliente/TarjetaCreditoController.java
@@ -6,6 +6,7 @@
 package controllers.cliente;
 
 import java.net.URL;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -29,9 +30,9 @@ public class TarjetaCreditoController implements Initializable {
     @FXML
     private TextField txtCVC;
     @FXML
-    private ComboBox cmbMes;
+    private ComboBox<Integer> cmbMes;
     @FXML
-    private ComboBox cmbAnios;
+    private ComboBox<Integer> cmbAnios;
     @FXML
     private Button btnPagar;
     @FXML
@@ -51,56 +52,52 @@ public class TarjetaCreditoController implements Initializable {
         cmbHorarios.getItems().add("14:00 - 14:30");
         cmbHorarios.getItems().add("14:30 - 15:00");
         
-        cmbMes.getItems().add("1");
-        cmbMes.getItems().add("2");
-        cmbMes.getItems().add("3");
-        cmbMes.getItems().add("4");
-        cmbMes.getItems().add("5");
-        cmbMes.getItems().add("6");
-        cmbMes.getItems().add("7");
-        cmbMes.getItems().add("8");
-        cmbMes.getItems().add("9");
-        cmbMes.getItems().add("10");
-        cmbMes.getItems().add("11");
-        cmbMes.getItems().add("12");
+        cmbMes.getItems().add(1);
+        cmbMes.getItems().add(2);
+        cmbMes.getItems().add(3);
+        cmbMes.getItems().add(4);
+        cmbMes.getItems().add(5);
+        cmbMes.getItems().add(6);
+        cmbMes.getItems().add(7);
+        cmbMes.getItems().add(8);
+        cmbMes.getItems().add(9);
+        cmbMes.getItems().add(10);
+        cmbMes.getItems().add(11);
+        cmbMes.getItems().add(12);
         
-        cmbAnios.getItems().add("2017");
-        cmbAnios.getItems().add("2018");
-        cmbAnios.getItems().add("2019");
-        cmbAnios.getItems().add("2020");
-        cmbAnios.getItems().add("2021");
-        cmbAnios.getItems().add("2022");
-        cmbAnios.getItems().add("2023");
-        cmbAnios.getItems().add("2024");
-        cmbAnios.getItems().add("2025");
+        cmbAnios.getItems().add(2017);
+        cmbAnios.getItems().add(2018);
+        cmbAnios.getItems().add(2019);
+        cmbAnios.getItems().add(2020);
+        cmbAnios.getItems().add(2021);
+        cmbAnios.getItems().add(2022);
+        cmbAnios.getItems().add(2023);
+        cmbAnios.getItems().add(2024);
+        cmbAnios.getItems().add(2025);
     }    
     
     @FXML
     private void pagar(ActionEvent event) {
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        Date fecha = new Date();
-        if (fecha.getYear() > (int)cmbAnios.getValue() && txtTarjeta.getText().length() > 12 && txtTarjeta.getText().matches("\\d*")){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Compra por tarjeta");
-            alert.setHeaderText("Ingreso de tarjeta Válido");
-            alert.setContentText("Su orden ha sido cargada a su tarjeta de Crédito");
-            alert.showAndWait();
-        }else if (fecha.getYear() == (int)cmbAnios.getValue() && fecha.getMonth() > (int)cmbMes.getValue() && 
-                    txtTarjeta.getText().length() > 12 && txtTarjeta.getText().matches("\\d*")){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Compra por tarjeta");
-            alert.setHeaderText("Ingreso de tarjeta Válido");
-            alert.setContentText("Su orden ha sido cargada a su tarjeta de Crédito");
-            alert.showAndWait();
-        }else{
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Error de ingreso tarjeta");
-            alert.setHeaderText("Error al Ingresar Tarjeta :(");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Compra por tarjeta");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        int year = cal.get(Calendar.YEAR);
+        int mes = cal.get(Calendar.MONTH);
+        if (txtTarjeta.getText().length() != 12 && txtTarjeta.getText().matches("\\d*")){
+            alert.setHeaderText("Ingreso de tarjeta no válido");
+            alert.setContentText("Ingrese un número de tarjeta válido");
+        }else if (txtCVC.getText().length() != 3 && txtTarjeta.getText().matches("\\d*")){
+            alert.setHeaderText("Ingreso de tarjeta no válido");
+            alert.setContentText("Ingreso de CVC no válido");
+        }else if (cmbAnios.getValue() < year || (cmbAnios.getValue() == year && cmbMes.getValue() < mes)){
+            alert.setHeaderText("Fecha de expiracion");
             alert.setContentText("Su tarjeta ha expirado");
-            alert.showAndWait();
+        }else{
+            alert.setHeaderText("Ingreso de tarjeta válido");
+            alert.setContentText("Su orden ha sido cargada a su tarjeta de crédito");
         }
-            
-         
+        alert.showAndWait();
     }
     
 }
